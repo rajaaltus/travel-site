0a572ae035cff258ba3acd7893d308cd06199558
Raja S <rajas@Rajas-MacBook-2.local>
2017-10-16 10:38:44 +0700

    header completed for desktop and mobile

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -21,6 +21,10 @@ gulp.task('watch', function(){
 
 		gulp.start('cssInject');
 
+	});
+
+	watch('./app/assets/scripts/**/*.js', function() {
+		gulp.start('scriptsRefresh');
 	})
 
 });
@@ -30,4 +34,9 @@ gulp.task('cssInject', ['styles'],function(){
 	return gulp.src('./app/css/styles/styles.css')
 		.pipe(browserSync.stream());
 
+});
+
+gulp.task('scriptsRefresh', ['scripts'], function() {
+
+	browserSync.reload();
 });
