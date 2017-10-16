cd01cbf35e16a45c98c799304c55f28ef753cd12
Raja S <rajas@Rajas-MacBook-2.local>
2017-10-16 18:21:47 +0700

    completed stickyHeader

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,14 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 
  var mobileMenu = new MobileMenu();
+ var stickyHeader = new StickyHeader();
 
  new RevealOnScroll($(".feature-item"),"85%");
  new RevealOnScroll($(".testimonial"),"60%");
  new RevealOnScroll($(".site-footer"),"100%");
+
+ 
+
